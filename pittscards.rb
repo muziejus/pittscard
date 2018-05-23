16ef2f7f09343737c954f6ce0f91b60b551fb898
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-23 14:41:43 -0400

    Get search and phrase building to work.

--- pittscards.rb
+++ pittscards.rb
@@ -14,7 +14,6 @@ class Pittscard
 
   def tweet
     create_pittscard
-    start_client
     # puts @tweet
     # puts @tweet.length
     # puts "See above"
@@ -27,14 +26,44 @@ class Pittscard
   end
 
   def create_tweet
-    @tweet = "text"
+    # start_client
+    # @tweet = "text"
+    puts set_phrase
   end
 
   def create_image
     original_photo = grab_image_of_pittsburgh
+
   end
     
   # private 
+  def set_phrase
+    search_results = search_twitter
+    # Now refine the results. Strip anything that doesn't have "Pittsburgh is" in it
+    tweets = search_results.select{|t| t.match(/Pittsburgh is/)}
+    # further refinement: turn "http://" & "\n" into "!" so it stops trimming
+    # and then also trim everything before the "Pittsburgh is" phrase
+    tweets = tweets.map{|t| t.gsub(/(\n|http)/, "!")}.map{|t| t.sub(/^.*Pittsburgh is/, "Pittsburgh is")}
+    # now trim and add exclamation point.
+    tweets = tweets.map{|t| t.sub(/(Pittsburgh is[^—".!]*)(.*)/, '\1').strip + "!"}
+    phrase = nil
+    while phrase.nil?
+      candidate = tweets.sample
+      # this could run forever… but it's not likely.
+      if blacklist.select{|badword| candidate.downcase.include? badword }.length == 0 && candidate.length < 65
+        phrase = candidate
+      end
+    end
+    @phrase = phrase
+  end
+
+  def search_twitter
+    #@client.search('"Pittsburgh is"', { result_type: "recent" }).map{ |tweet| tweet.full_text }
+    results = []
+    File.readlines("text.txt").each { |f| results << f.strip }
+    results
+  end
+
   def grab_image_of_pittsburgh
     # Pull 500 images from Wikipedia associated w/ Pittsburgh
     # and filter out the ones end in "svg" or "tif" and the ones
@@ -51,16 +80,16 @@ class Pittscard
     filename
   end
 
-  def get_photo_from_wiki(url)
+  def get_photo_from_wiki url
     # Wiki makes a page out of the file, so we need to get the file's url
     wiki_page = Nokogiri::HTML open(url)
     # Then get the link from the .fullMedia div
     wiki_page.at_css(".fullMedia").at_css("a")["href"].sub(/^/, "https:")
   end
 
-  def get_blacklist
+  def blacklist
     # Grab the list of bad words from Darius.
-    @blacklist = JSON.parse(HTTParty.get("https://raw.githubusercontent.com/dariusk/wordfilter/master/lib/badwords.json").body)
+    JSON.parse(HTTParty.get("https://raw.githubusercontent.com/dariusk/wordfilter/master/lib/badwords.json").body)
   end
 
   def start_client
@@ -83,4 +112,4 @@ class Pittscard
 end
 
 card = Pittscard.new
-card.create_image
+card.create_pittscard
